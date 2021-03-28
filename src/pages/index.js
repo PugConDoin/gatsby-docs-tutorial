db57af2960011eaa27fc39088bcaf858286277ab
Mark Freeman <dorsettri@gmail.com>
2021-03-28 17:15:11 +0100

    add contact, links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <h1>Hello Gatsby!</h1>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
       <p>What a world</p>
       <img
         src="https://images.unsplash.com/photo-1592513002316-e4fa19175023?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjN8fGF2ZW5nZXJzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
