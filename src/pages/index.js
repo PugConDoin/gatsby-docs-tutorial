8c989eb7ca415b1a3735724f8193b2e6d2b7352a
Mark Freeman <dorsettri@gmail.com>
2021-03-28 17:08:05 +0100

    add about, header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello Gatsby!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world</p>
+      <img
+        src="https://images.unsplash.com/photo-1592513002316-e4fa19175023?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjN8fGF2ZW5nZXJzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+        alt=""
+      />
+    </div>
+  )
 }
